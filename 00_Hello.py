7b214dc4bae2cbf934747264120cb0227b72102d
JorgeAgulloM <agullojorge@gmail.com>
2022-11-18 22:04:23 +0100

    New commit

---
+++ 00_Hello.py
@@ -0,0 +1,18 @@
+#This is a comment: Hello World!
+print("Hello Python!")
+print('Hello Python!')
+
+#Print data type
+print(type('Hello Python!')) #this type is class str
+print(type(10)) #this type is class int
+print(type(10.5)) #this type is class float
+print(type(3 + 1j)) #this type is class complex
+print(type(True)) #this type is class bool
+
+
+
+"""
+This is
+a comment
+in 3 lines
+"""
---
+++ 01_Variables.py
@@ -0,0 +1,43 @@
+#Variables
+my_string_variable = 'im one string varible'
+my_int_variable = 10
+my_float_variable = 10.0
+my_bool_variable = True
+
+print(my_string_variable)
+print(my_int_variable)
+print(my_float_variable)
+print(my_bool_variable)
+
+# Concatenate variables
+print(my_string_variable, my_int_variable, my_bool_variable)
+print("My values is:", my_int_variable)
+
+# Parese int to string
+my_int_to_str_variable = str(my_int_variable)
+print(my_int_variable)
+print(my_int_to_str_variable)
+print(type(my_int_to_str_variable)) #Im a class str
+
+# System funtion eg
+print(len(my_string_variable)) #Count of string in this case
+
+# Variables in one line ¡¡USE WITH CARE!!
+name, surname, alias, age = "Jorge", 'Agullo', "Yorch", 39
+print("My name is", name, surname, ",my alias is", alias, "and my age is", age)
+
+# Input
+name = input('what is your name?')
+age = input('what is your age?')
+
+print(name)
+print(age)
+
+# Change types
+name = 39
+age = "Jorge"
+
+print(name)
+print(age)
+
+
